3cd1957b35981ad298d22062072643f29ae235e4
Kishore <ks.kishore59@gmail.com>
2022-01-23 01:01:37 +0000

    solution

--- app.js
+++ app.js
@@ -4,9 +4,16 @@ const sqlite3 = require("sqlite3");
 const path = require("path");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const cors = require("cors");
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 
 const app = express();
 app.use(express.json());
+app.use(cors(corsOptions)); // Use this after the variable declaration
 
 const databasePath = path.join(__dirname, "financePeer.db");
 
